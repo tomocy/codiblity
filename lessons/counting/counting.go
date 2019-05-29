1c272f48b9ce8501d2f5516b21b4b02bca7911d5
tomocy <tomocy.dev@gmail.com>
2019-05-29 19:06:27 +0900

    add some solutions - time frog jumps over river

--- lessons/counting/counting.go
+++ lessons/counting/counting.go
@@ -1,6 +1,9 @@
 package counting
 
-import "sort"
+import (
+	"errors"
+	"sort"
+)
 
 func isPermutation(as []int) bool {
 	sorted := sortByAsc(as)
@@ -22,4 +25,35 @@ func sortByAsc(as []int) []int {
 	sort.Sort(sort.IntSlice(sorted))
 
 	return sorted
+}
+
+func timeFrogJumpOverRiver(x int, as []int) (int, error) {
+	felt := make(map[int]bool)
+	for i := 1; i <= x; i++ {
+		felt[i] = false
+	}
+	for i, a := range as {
+		if x < a {
+			continue
+		}
+
+		if !felt[a] {
+			felt[a] = true
+		}
+		if isAbleToJumpOverRiver(felt) {
+			return i, nil
+		}
+	}
+
+	return 0, errors.New("could not jump over river")
+}
+
+func isAbleToJumpOverRiver(ready map[int]bool) bool {
+	for _, v := range ready {
+		if !v {
+			return false
+		}
+	}
+
+	return true
 }
--- lessons/counting/counting_test.go
+++ lessons/counting/counting_test.go
@@ -29,3 +29,41 @@ func TestIsPermutation(t *testing.T) {
 		})
 	}
 }
+
+func TestTimeFrogJumpsOverRiver(t *testing.T) {
+	type input struct {
+		x  int
+		as []int
+	}
+	type expected struct {
+		cnt int
+		err error
+	}
+	tests := []struct {
+		input    input
+		expected expected
+	}{
+		{
+			input{
+				5,
+				[]int{1, 3, 1, 4, 2, 3, 5, 4},
+			},
+			expected{
+				6,
+				nil,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%v", test.input), func(t *testing.T) {
+			actual, err := timeFrogJumpOverRiver(test.input.x, test.input.as)
+			if err != test.expected.err {
+				t.Errorf("err: got %v, but expected %v\n", err, test.expected.err)
+			}
+			if actual != test.expected.cnt {
+				t.Errorf("cnt: got %d, but expected %d\n", actual, test.expected.cnt)
+			}
+		})
+	}
+}
