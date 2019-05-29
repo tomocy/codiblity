4d3dea15d9ed8e5c2be89e2d57ff6e367b8b86a4
tomocy <tomocy.dev@gmail.com>
2019-05-29 19:11:33 +0900

    add some solutions - find minimal impact factors

--- lessons/sum/sum.go
+++ lessons/sum/sum.go
@@ -35,3 +35,52 @@ func isCarTravelingToWest(n int) bool {
 func isCarTravelingToEast(n int) bool {
 	return n == 0
 }
+
+func findMinimalImpactFactors(s string, ps []int, qs []int) []int {
+	factors := make([]int, len(ps))
+	for i := range ps {
+		begin, end := ps[i], qs[i]
+		part := s[begin : end+1]
+		categ := categorizeNucleotides(part)
+		factors[i] = findMinimalImpactFactor(categ)
+	}
+
+	return factors
+}
+
+func categorizeNucleotides(s string) category {
+	category := make(category)
+	for _, r := range s {
+		category[string(r)]++
+	}
+
+	return category
+}
+
+func findMinimalImpactFactor(categ category) int {
+	var i, min int
+	for nuc := range categ {
+		prevI := i
+		i++
+		current := factors[nuc]
+		if prevI == 0 {
+			min = current
+			continue
+		}
+
+		if current < min {
+			min = current
+		}
+	}
+
+	return min
+}
+
+var factors = map[string]int{
+	"A": 1,
+	"C": 2,
+	"G": 3,
+	"T": 4,
+}
+
+type category map[string]int
--- lessons/sum/sum_test.go
+++ lessons/sum/sum_test.go
@@ -33,3 +33,36 @@ func TestCountPassingCars(t *testing.T) {
 		})
 	}
 }
+
+func TestFindMinimalImpactFactors(t *testing.T) {
+	type input struct {
+		s  string
+		ps []int
+		qs []int
+	}
+	tests := []struct {
+		input    input
+		expected []int
+	}{
+		{
+			input{
+				"CAGCCTA", []int{2, 5, 0}, []int{4, 5, 6},
+			},
+			[]int{2, 4, 1},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%v", test.input), func(t *testing.T) {
+			actual := findMinimalImpactFactors(test.input.s, test.input.ps, test.input.qs)
+			if len(actual) != len(test.expected) {
+				t.Fatalf("len: got %d, but expected %d\n", len(actual), len(test.expected))
+			}
+			for i := range test.expected {
+				if actual[i] != test.expected[i] {
+					t.Errorf("at %d: got %d, but expected %d\n", i, actual[i], test.expected[i])
+				}
+			}
+		})
+	}
+}
