7f818585acc6383e3f9d05294d8c3b7bd138fd39
tomocy <tomocy.dev@gmail.com>
2019-05-31 18:56:59 +0900

    add some solutions - can build trianble - count disc intersects

--- lessons/sort/sort.go
+++ lessons/sort/sort.go
@@ -1,6 +1,8 @@
 package sort
 
-import "sort"
+import (
+	"sort"
+)
 
 func countDistincts(as []int) int {
 	dists := make(map[int]int)
@@ -22,10 +24,87 @@ func findMaximalProduct(as []int) int {
 	return largest3 * largest2 * largest
 }
 
+func canBuildTrianble(as []int) bool {
+	if len(as) < 3 {
+		return false
+	}
+
+	sorted := sortByAsc(as)
+	for i := 0; i <= len(sorted)-3; i++ {
+		if sorted[i+2] <= 0 {
+			continue
+		}
+		if sorted[i+2] < sorted[i]+sorted[i+1] {
+			return true
+		}
+	}
+
+	return false
+}
+
 func sortByAsc(as []int) []int {
 	sorted := make([]int, len(as))
 	copy(sorted, as)
 	sort.Ints(sorted)
 
 	return sorted
+}
+
+func countDiscIntersects(discs []*disc) int {
+	var cnt int
+	sorted := sortByDiameter(discs)
+	for i, disc := range sorted {
+		if len(sorted)-1 <= i {
+			break
+		}
+		for _, target := range sorted[i+1:] {
+			if disc.doIntersect(target) {
+				cnt++
+			}
+		}
+	}
+
+	return cnt
+}
+
+func sortByDiameter(src []*disc) []*disc {
+	sorted := make(byDiameter, len(src))
+	copy(sorted, src)
+	sort.Sort(sorted)
+
+	return sorted
+}
+
+type byDiameter []*disc
+
+func (d byDiameter) Len() int {
+	return len(d)
+}
+
+func (d byDiameter) Less(i, j int) bool {
+	return d[i].diameter() < d[j].diameter()
+}
+
+func (d byDiameter) Swap(i, j int) {
+	d[i], d[j] = d[j], d[i]
+}
+
+type disc struct {
+	left, right point
+}
+
+type point struct {
+	x int
+}
+
+func (d *disc) diameter() int {
+	return d.right.x - d.left.x
+}
+
+func (d *disc) doIntersect(target *disc) bool {
+	if target.right.x < d.left.x || d.right.x < target.left.x {
+		return false
+	}
+
+	return true
 }
--- lessons/sort/sort_test.go
+++ lessons/sort/sort_test.go
@@ -50,3 +50,56 @@ func TestFindMaximalProduct(t *testing.T) {
 		})
 	}
 }
+
+func TestCanBuildTrianble(t *testing.T) {
+	tests := []struct {
+		input    []int
+		expected bool
+	}{
+		{
+			[]int{10, 2, 5, 1, 8, 20},
+			true,
+		},
+		{
+			[]int{10, 50, 5, 1},
+			false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%v", test.input), func(t *testing.T) {
+			actual := canBuildTrianble(test.input)
+			if actual != test.expected {
+				t.Errorf("got %t, but expected %t\n", actual, test.expected)
+			}
+		})
+	}
+}
+
+func TestCountDiscIntersects(t *testing.T) {
+	tests := []struct {
+		input    []*disc
+		expected int
+	}{
+		{
+			[]*disc{
+				&disc{point{-1}, point{1}},
+				&disc{point{-4}, point{6}},
+				&disc{point{0}, point{4}},
+				&disc{point{2}, point{4}},
+				&disc{point{0}, point{8}},
+				&disc{point{5}, point{5}},
+			},
+			11,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%v", test.input), func(t *testing.T) {
+			actual := countDiscIntersects(test.input)
+			if actual != test.expected {
+				t.Errorf("got %d, but expected %d\n", actual, test.expected)
+			}
+		})
+	}
+}
