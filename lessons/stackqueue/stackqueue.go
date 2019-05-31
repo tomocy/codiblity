c5b088c042595b551ea56e02012dbc62a8fe40b2
tomocy <tomocy.dev@gmail.com>
2019-06-01 02:25:06 +0900

    add some solutions - count fishes alive - count blocks for stone wall

--- lessons/stackqueue/stackqueue.go
+++ lessons/stackqueue/stackqueue.go
@@ -32,3 +32,100 @@ var parens = map[string]string{
 	"{": "}",
 	"(": ")",
 }
+
+func countFishesAlive(fishes []*fish) int {
+	var ups, downs fishStack
+	for _, fish := range fishes {
+		if fish.direction == downstream {
+			downs.push(fish)
+			continue
+		}
+
+		for 0 < len(downs) {
+			down := downs.peek()
+			if down.canEat(fish) {
+				break
+			}
+
+			downs.pop()
+		}
+		if len(downs) == 0 {
+			ups.push(fish)
+		}
+	}
+
+	return len(append(ups, downs...))
+}
+
+type fishStack []*fish
+
+func (fs *fishStack) push(fish *fish) {
+	*fs = append(*fs, fish)
+}
+
+func (fs *fishStack) pop() *fish {
+	peeked := fs.peek()
+	*fs = (*fs)[:len(*fs)-1]
+
+	return peeked
+}
+
+func (fs *fishStack) peek() *fish {
+	return (*fs)[len(*fs)-1]
+}
+
+type fish struct {
+	position  int
+	size      int
+	direction direction
+}
+
+const (
+	_ direction = iota
+	upstream
+	downstream
+)
+
+type direction int
+
+func (f *fish) canEat(target *fish) bool {
+	return target.size < f.size
+}
+
+func countBlocksForStoneWall(blocks []*block) int {
+	var useds blockStack
+	var cnt int
+	for _, block := range blocks {
+		for 0 < len(useds) && block.height < useds.peek().height {
+			useds.pop()
+		}
+
+		if len(useds) == 0 || useds.peek().height < block.height {
+			useds.push(block)
+			cnt++
+		}
+	}
+
+	return cnt
+}
+
+type blockStack []*block
+
+func (bs *blockStack) push(block *block) {
+	*bs = append(*bs, block)
+}
+
+func (bs *blockStack) pop() *block {
+	peeked := bs.peek()
+	*bs = (*bs)[:len(*bs)-1]
+
+	return peeked
+}
+
+func (bs *blockStack) peek() *block {
+	return (*bs)[len(*bs)-1]
+}
+
+type block struct {
+	height int
+}
--- lessons/stackqueue/stackqueue_test.go
+++ lessons/stackqueue/stackqueue_test.go
@@ -23,3 +23,87 @@ func TestIsProperlyNested(t *testing.T) {
 		})
 	}
 }
+
+func TestCountAlivingFishes(t *testing.T) {
+	tests := []struct {
+		input    []*fish
+		expected int
+	}{
+		{
+			[]*fish{
+				&fish{0, 4, upstream},
+				&fish{1, 3, downstream},
+				&fish{2, 2, upstream},
+				&fish{3, 1, upstream},
+				&fish{4, 5, upstream},
+			},
+			2,
+		},
+		{
+			[]*fish{
+				&fish{0, 4, upstream},
+				&fish{1, 3, downstream},
+				&fish{2, 2, downstream},
+				&fish{3, 1, upstream},
+				&fish{4, 5, upstream},
+			},
+			2,
+		},
+		{
+			[]*fish{
+				&fish{0, 4, upstream},
+				&fish{1, 3, upstream},
+				&fish{2, 2, upstream},
+			},
+			3,
+		},
+		{
+			[]*fish{
+				&fish{0, 4, downstream},
+				&fish{1, 3, downstream},
+				&fish{2, 2, downstream},
+			},
+			3,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%v", test.input), func(t *testing.T) {
+			actual := countFishesAlive(test.input)
+			if actual != test.expected {
+				t.Errorf("got %d, but expected %d\n", actual, test.expected)
+			}
+		})
+	}
+}
+
+func TestCountBlocksForStoneWall(t *testing.T) {
+	tests := []struct {
+		input    []*block
+		expected int
+	}{
+		{
+			[]*block{
+				&block{8},
+				&block{8},
+				&block{5},
+				&block{7},
+				&block{9},
+				&block{8},
+				&block{7},
+				&block{4},
+				&block{8},
+			},
+			7,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%v", test.input), func(t *testing.T) {
+			actual := countBlocksForStoneWall(test.input)
+			if actual != test.expected {
+				t.Errorf("got %d, but expected %d\n", actual, test.expected)
+			}
+		})
+	}
+}
