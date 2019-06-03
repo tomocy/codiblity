b2560205464136a2e0bb6ac0880a37080cdd2568
tomocy <tomocy.dev@gmail.com>
2019-06-03 19:13:46 +0900

    add some solutions - find maximum sum of double

--- lessons/maxslice/maxslice.go
+++ lessons/maxslice/maxslice.go
@@ -1,5 +1,7 @@
 package maxslice
 
+import "math"
+
 func findMaximumProfit(as []int) int {
 	var min, max int
 	for i, a := range as {
@@ -11,6 +13,44 @@ func findMaximumProfit(as []int) int {
 		if max < current {
 			max = current
 		}
+	}
+
+	return max
+}
+
+func findMaximumSum(as []int) int {
+	var maxAs, max int
+	for i, a := range as {
+		maxAs += a
+		if maxAs < a {
+			maxAs = a
+		}
+
+		if i == 0 || max < maxAs {
+			max = maxAs
+		}
+	}
+
+	return max
+}
+
+func findMaximumSumOfDouble(as []int) int {
+	if len(as) <= 3 {
+		return 0
+	}
+
+	n := len(as) - 2
+	lefts, rights := make([]int, n), make([]int, n)
+	for i := 0; i < n-1; i++ {
+		left, right := as[i+1], as[n-i]
+		front, back := i+1, n-2-i
+		lefts[front] = int(math.Max(0, float64(left+lefts[front-1])))
+		rights[back] = int(math.Max(0, float64(right+rights[back+1])))
+	}
+
+	max := lefts[0] + rights[0]
+	for i := 1; i < len(lefts); i++ {
+		max = int(math.Max(float64(max), float64(lefts[i]+rights[i])))
 	}
 
 	return max
--- lessons/maxslice/maxslice_test.go
+++ lessons/maxslice/maxslice_test.go
@@ -25,3 +25,61 @@ func TestFindMaximumProfit(t *testing.T) {
 		})
 	}
 }
+
+func TestFindMaximumSum(t *testing.T) {
+	tests := []struct {
+		input    []int
+		expected int
+	}{
+		{
+			[]int{1, 3},
+			4,
+		},
+		{
+			[]int{3, 2, -6, 4, 0},
+			5,
+		},
+		{
+			[]int{10},
+			10,
+		},
+		{
+			[]int{-10},
+			-10,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%v", test.input), func(t *testing.T) {
+			actual := findMaximumSum(test.input)
+			if actual != test.expected {
+				t.Errorf("got %d, but expected %d\n", actual, test.expected)
+			}
+		})
+	}
+}
+
+func TestFindMaximumSumOfDouble(t *testing.T) {
+	tests := []struct {
+		input    []int
+		expected int
+	}{
+		{
+			[]int{3, 2, 6, -1, 4, 5, -1, 2},
+			17,
+		},
+		{
+			[]int{0, 10, -5, -2, 0},
+			10,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%v", test.input), func(t *testing.T) {
+			actual := findMaximumSumOfDouble(test.input)
+			if actual != test.expected {
+				t.Errorf("got %d, but expected %d\n", actual, test.expected)
+			}
+		})
+	}
+}
